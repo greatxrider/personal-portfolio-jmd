a339dbd09e3341dd6d1f4c296c5d3a6b3f3af33a
Jeph Mari Daligdig <122143538+greatxrider@users.noreply.github.com>
2023-10-01 18:54:11 +0800

    uploading tailwind

---
+++ tailwind/src/script.js
@@ -0,0 +1,9 @@
+// Initialization for ES Users
+import {
+    Carousel,
+    Collapse,
+    Dropdown,
+    initTE,
+} from "../node_modules/tw-elements/dist/js/tw-elements.es.min.js";
+
+initTE({ Collapse, Dropdown, Carousel });
